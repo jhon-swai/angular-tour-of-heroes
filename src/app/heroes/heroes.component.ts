3130e2f0901fa9e540034e4a0de9375976391056
jhon-swai <jhonswai@gmail.com>
2019-10-29 15:09:29 +0300

    binding an event on the heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,11 +9,19 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero ={
-    id :1,
-    name: 'Windstorm'
-  };
+  // Renaming the property
+  // hero: Hero ={
+  //   id :1,
+  //   name: 'Windstorm'
+  // };
+  selectedHero: Hero;
   heroes = HEROES;
+  // Method to handle selected hero
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+
+  }
+
 
   constructor() { }
 
